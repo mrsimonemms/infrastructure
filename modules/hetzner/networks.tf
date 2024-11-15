21cf9bdcdd9280dab01c4bbdc0e8c936a76ff11d
Simon Emms <simon@simonemms.com>
2024-11-15 14:44:17 +0000

    feat: add unifi controller

--- modules/hetzner/networks.tf
+++ modules/hetzner/networks.tf
@@ -12,6 +12,105 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+locals {
+  firewall = [
+    {
+      description = "SSH port"
+      port        = var.ssh_port
+      source_ips  = var.firewall_allow_ssh_access
+    },
+    {
+      description = "Allow ICMP (ping)"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      protocol = "icmp"
+      port     = null
+    },
+    {
+      description = "Allow all TCP traffic on private network"
+      source_ips = [
+        hcloud_network.network.ip_range
+      ]
+    },
+    {
+      description = "Allow all UDP traffic on private network"
+      source_ips = [
+        hcloud_network.network.ip_range
+      ]
+      protocol = "udp"
+    },
+    {
+      description = "Allow TCP access to port 80"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port = 80
+    },
+    {
+      description = "Allow TCP access to port 443"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port = 443
+    },
+    # Unifi ports
+    {
+      description = "Unifi controller"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port = 8080
+    },
+    {
+      description = "Unifi speedtest"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port = 6789
+    },
+    {
+      description = "Unifi stun"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port     = 3478
+      protocol = "udp"
+    },
+    {
+      description = "Unifi syslog"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port     = 5514
+      protocol = "udp"
+    },
+    {
+      description = "Unifi discovery"
+      source_ips = [
+        local.global_ipv4_cidr,
+        local.global_ipv6_cidr,
+      ]
+      port     = 10001
+      protocol = "udp"
+    },
+    # Direct public access only allowed if single manager node
+    {
+      description = "Allow access to Kubernetes API"
+      port        = local.kubernetes_api_port
+      source_ips  = var.firewall_allow_api_access
+      disabled    = var.k3s_manager_pool.count > 1
+    }
+  ]
+}
+
 resource "hcloud_network" "network" {
   name     = format(local.name_format, "network")
   ip_range = var.network_subnet
@@ -30,58 +129,7 @@ resource "hcloud_firewall" "firewall" {
   name = format(local.name_format, "firewall")
 
   dynamic "rule" {
-    for_each = [for each in [
-      {
-        description = "SSH port"
-        port        = var.ssh_port
-        source_ips  = var.firewall_allow_ssh_access
-      },
-      {
-        description = "Allow ICMP (ping)"
-        source_ips = [
-          local.global_ipv4_cidr,
-          local.global_ipv6_cidr,
-        ]
-        protocol = "icmp"
-        port     = null
-      },
-      {
-        description = "Allow all TCP traffic on private network"
-        source_ips = [
-          hcloud_network.network.ip_range
-        ]
-      },
-      {
-        description = "Allow all UDP traffic on private network"
-        source_ips = [
-          hcloud_network.network.ip_range
-        ]
-        protocol = "udp"
-      },
-      {
-        description = "Allow TCP access to port 80"
-        source_ips = [
-          local.global_ipv4_cidr,
-          local.global_ipv6_cidr,
-        ]
-        port = 80
-      },
-      {
-        description = "Allow TCP access to port 443"
-        source_ips = [
-          local.global_ipv4_cidr,
-          local.global_ipv6_cidr,
-        ]
-        port = 443
-      },
-      # Direct public access only allowed if single manager node
-      {
-        description = "Allow access to Kubernetes API"
-        port        = local.kubernetes_api_port
-        source_ips  = var.firewall_allow_api_access
-        disabled    = var.k3s_manager_pool.count > 1
-      }
-    ] : each if lookup(each, "disabled", false) != true]
+    for_each = [for each in local.firewall : each if lookup(each, "disabled", false) != true]
 
     content {
       description     = lookup(rule.value, "description", "")
